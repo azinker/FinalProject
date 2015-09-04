b32bc225e1fabb734661ea3008ca1f713f9ff0b4
Zinker <azforcefed@gmail.com>
2015-09-04 14:47:17 -0700

    fixed moltin api

--- public/js/services/moltin.js
+++ public/js/services/moltin.js
@@ -1,7 +1,7 @@
 angular.module('storefrontApp.moltin', [])
   .factory('MoltinAuth', function($q) {
     var deferred = $q.defer();
-    var moltin = new Moltin({publicId: 'hHr6QhXDTz9XigXiyPzKaaeLEHLHJJXhychmd186'});
+    var moltin = new Moltin({publicId: 'mipDNUidZJx1iEOjibmySAjOU3MF5XRwPYTPkJt4'});
     moltin.Authenticate(function() {
       deferred.resolve(moltin);
     });
